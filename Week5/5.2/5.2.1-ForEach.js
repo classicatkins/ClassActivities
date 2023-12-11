9201cf648d6bf04ce854b9b765078f51c531524a
Melissa Atkins <classicatkins@gmail.com>
2023-12-10 21:58:35 -0600

    done

--- Week5/5.2/5.2.1-ForEach.js
+++ Week5/5.2/5.2.1-ForEach.js
@@ -258,7 +258,9 @@ phrase.push(`${curr.company.catchPhrase} ${curr.company.bs}`)
 console.log(phrase);
 
 // query the HTML for the class class-week, then use forEach to change the text content
-const classWeekElements = document.querySelectorAll('.class-week');
-classWeekElements.forEach(elem => {
-  elem.textContent = 'New Text';
+const classes = document.querySelectorAll(".class-week");
+console.log(classes);
+
+classes.forEach((htmlElement, i) => {
+  htmlElement.textContent = `Text content has changed, Class Week is ${i}`;
 });
